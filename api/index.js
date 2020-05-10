9647b9d217a5785cc3adfa5a1a502f796d7552b1
adrian <adrianbogado67@gmail.com>
2020-05-10 17:22:35 -0300

    cambio

--- api/index.js
+++ api/index.js
@@ -1,28 +1,18 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-require("dotenv").config()
-
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.set("port", 3000)
-app.use(bodyParser.json())
+mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+const Users = mongoose.model('User', new mongoose.Schema({name:String}))
+Users.create({name:'CHANCHITO FELIZ'})
+
+
+app.get('*',(req,res) =>{
+    Users.find()
+    .then(x => res.send(x))
 })
 
-app.use("/api/meals", require("./routes/meals"))
-app.use("/api/orders", require("./routes/orders"))
-app.use("/api/auth", require("./routes/auth"))
 
-app.get("*", (req, res) => {
-	res.send("Page doesn't exists")
-})
-
-app.listen(app.get("port"), () => {
-	console.log(`Server on port ${app.get("port")}`)
-})
 
 module.exports = app
